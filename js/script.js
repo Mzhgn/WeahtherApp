9203940112737e948a567ea7e180250b92e4eb3a
Mozhgan <mojganshah@gmail.com>
2024-08-21 18:46:06 +0330

    arrays added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-﻿
+﻿"use strict";
+// let imagesArray=[{id:1,img:}]
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
