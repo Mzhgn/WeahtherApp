f9abd8730ec478ed34474ccd37b3c3ddb5f0d02b
Mozhgan <mojganshah@gmail.com>
2024-08-22 13:02:19 +0330

    fetch data launched

--- js/script.js
+++ js/script.js
@@ -24,3 +24,30 @@ let days = [
   "Friday",
   "Saturday",
 ];
+let $ = document;
+
+const inputElem = $.querySelector(".search-bar");
+const searchBtn = $.querySelector("#search");
+
+let apiKey = "3244187444f4b03034a273eot82c9c1e";
+
+function fetchData() {
+  let city = inputElem.value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      showData(data);
+    });
+}
+
+function showData(data) {}
+
+inputElem.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    fetchData();
+  }
+});
+searchBtn.addEventListener("click", fetchData);
